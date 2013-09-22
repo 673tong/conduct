5dbc81851df2072e36b86b6f291b07ebdb9f29df
鲁家宁 <facebooot@gmail.com>
2013-09-22 14:13:13 +0800

    add qianyan2

--- src/test/java/com/qianyan/Test.java
+++ src/test/java/com/qianyan/Test.java
@@ -11,5 +11,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello qianyan");
         System.out.println("hello qianyan1");
+        System.out.println("hello qianyan2");
     }
 }
